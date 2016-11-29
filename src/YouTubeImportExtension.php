af01fe719b9376c1428060987a280df14ca93177
Chris Hilsdon <chris@koolserve.uk>
2016-11-29 07:26:33 +0000

    Initial commit :rocket:

---
+++ src/YouTubeImportExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Bolt\Extension\Koolserve\YouTubeImport;
+
+use Bolt\Application;
+use Bolt\Extension\SimpleExtension;
+
+use Silex\ControllerCollection;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * YouTubeImport extension class.
+ *
+ * @author Chris Hilsdon <chris@koolserve.uk>
+ */
+class YouTubeImportExtension extends SimpleExtension
+{
+
+}
